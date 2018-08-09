8792a18dc7c860e86fe73c1b51c455334281d1a8
arturgspb <art@realweb.ru>
2018-08-10 00:37:38 +0300

    init

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-Based on https://gist.github.com/dpp-name/5d9553cddaaba8f7d3cc
+# Get started
 
+```
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+go get -u github.com/golang/protobuf/protoc-gen-go
+
+```
+
+
+Based on https://gist.github.com/dpp-name/5d9553cddaaba8f7d3cc
 
 https://github.com/grpc-ecosystem/awesome-grpc
 
 https://github.com/pseudomuto/protoc-gen-doc
+
+# Смотри: test-grpc
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #protoc --plugin=protoc-gen-demo=gen-impl.py --demo_out=. demo.proto
-protoc --python_out=./out --mypy_out=./out .defs/demo.proto
+#protoc --python_out=./out --mypy_out=./out ./defs/demo.proto
 
 protoc -I/usr/local/include -I. \
   -I$GOPATH/src \
@@ -7,7 +7,7 @@ protoc -I/usr/local/include -I. \
   --python_out=./out \
   --mypy_out=./out \
   --swagger_out=logtostderr=true:./out \
+  --grpc-gateway_out=logtostderr=true:./out \
   --go_out=plugins=grpc:./out \
   ./defs/demo.proto
 
-docker run -v `pwd`:/defs namely/protoc-all:1.11 -f demo.proto -l ruby
